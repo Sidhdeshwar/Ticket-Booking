b408fda323e0062d7459860debe940735c6dd3cd
Sidhdeshwar <sidhdeshwar@angularminds.in>
2023-07-06 19:56:42 +0530

    ASS

--- src/app/movie/movie-booking/movie-booking.component.ts
+++ src/app/movie/movie-booking/movie-booking.component.ts
@@ -8,6 +8,7 @@ import {bookingSchedule} from './../../store'
 export class MovieBookingComponent {
 
   Arr:any [] = [10,20,30,40,50,60,70,80,90,100,110,120];
+  public bookingSchedule1 = bookingSchedule
   TotalPrice:number = 0;
   public MySchedule = bookingSchedule.morning
 constructor()
@@ -22,16 +23,29 @@ changeTheSchedule(num:number)
 {
    if(num==1)
    {
+    bookingSchedule.morning.selected = true;
+    bookingSchedule.evening.selected = false;
+    bookingSchedule.afternoon.selected = false;
+
      this.MySchedule = bookingSchedule.morning
-     this.TotalPrice = bookingSchedule.morning.totalPrice
+     this.TotalPrice = bookingSchedule.morning.totalPrice;
+
    }
    if(num==2)
    {
+    bookingSchedule.morning.selected = false;
+    bookingSchedule.evening.selected = false;
+    bookingSchedule.afternoon.selected = true;
+
      this.MySchedule = bookingSchedule.afternoon;
      this.TotalPrice = bookingSchedule.afternoon.totalPrice;
    }
    if(num==3)
    {
+    bookingSchedule.morning.selected = false;
+    bookingSchedule.evening.selected = true;
+    bookingSchedule.afternoon.selected = false;
+
      this.MySchedule = bookingSchedule.evening
      this.TotalPrice = bookingSchedule.evening.totalPrice;
    }
